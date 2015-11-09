cbf3b5b8a9e86e594ca2f82521f394ddd81db29f
Mark Wassef <markdevw@gmail.com>
2015-11-09 16:48:43 -0500

    Refactored MessageItem

--- app/src/main/java/com/gmail/markdevw/wetube/api/model/MessageItem.java
+++ app/src/main/java/com/gmail/markdevw/wetube/api/model/MessageItem.java
@@ -7,16 +7,17 @@ public class MessageItem {
     public static final int INCOMING_MSG = 0;
     public static final int OUTGOING_MSG = 1;
 
-    String message;
-    int type;
+    private String mMessage;
+    private int mMessageType;
 
     public MessageItem(String message, int type){
         setMessage(message);
         setType(type);
     }
 
-    public void setMessage(String message) {this.message = message;}
-    public String getMessage() { return message; }
-    public void setType(int type) {this.type = type;}
-    public int getType() { return this.type; }
+    public void setMessage(String message) { this.mMessage = message; }
+    public String getMessage() { return mMessage; }
+    
+    public void setType(int type) {this.mMessageType = type;}
+    public int getType() { return this.mMessageType; }
 }

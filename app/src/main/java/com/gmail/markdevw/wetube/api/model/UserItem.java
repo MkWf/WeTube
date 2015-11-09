0719533848096801fb4b1d719bf6f46055123d29
Mark Wassef <markdevw@gmail.com>
2015-11-09 16:21:10 -0500

    Refactored UserItem

--- app/src/main/java/com/gmail/markdevw/wetube/api/model/UserItem.java
+++ app/src/main/java/com/gmail/markdevw/wetube/api/model/UserItem.java
@@ -4,11 +4,16 @@ package com.gmail.markdevw.wetube.api.model;
  * Created by Mark on 4/3/2015.
  */
 public class UserItem {
-    String name;
-    String id;
-    boolean isInSession;
-    boolean isLoggedIn;
-    boolean isFriend;
+    private String mName;
+    private String mId;
+    private boolean mIsInSession;
+    private boolean mIsLoggedIn;
+    private boolean mIsFriend;
+
+    public UserItem(String name, String id){
+        setName(name);
+        setId(id);
+    }
 
     public UserItem(String name, String id, boolean isInSession, boolean isLoggedIn, boolean isFriend){
         setName(name);
@@ -18,20 +23,19 @@ public class UserItem {
         setFriendStatus(isFriend);
     }
 
-    public UserItem(String name, String id){
-        setName(name);
-        setId(id);
-    }
+    public String getName() { return mName;}
+    public void setName(String name) { this.mName = name; }
 
-    public String getName() { return name;}
-    public void setName(String name) {this.name = name; }
-    public String getId() { return id; }
-    public void setId(String id) {this.id = id; }
-    public boolean getSessionStatus() { return isInSession; }
-    public void setSessionStatus(boolean isInSession) {this.isInSession = isInSession; }
-    public boolean getOnlineStatus() { return isLoggedIn; }
-    public void setOnlineStatus(boolean isLoggedIn) {this.isLoggedIn = isLoggedIn; }
-    public void setFriendStatus(boolean isFriend) {this.isFriend = isFriend;}
-    public boolean getFriendStatus() { return this.isFriend; }
+    public String getId() { return mId; }
+    public void setId(String id) {this.mId = id; }
+
+    public boolean getSessionStatus() { return mIsInSession; }
+    public void setSessionStatus(boolean isInSession) {this.mIsInSession = isInSession; }
+
+    public boolean getOnlineStatus() { return mIsLoggedIn; }
+    public void setOnlineStatus(boolean isLoggedIn) {this.mIsLoggedIn = isLoggedIn; }
+    
+    public void setFriendStatus(boolean isFriend) {this.mIsFriend = isFriend;}
+    public boolean getFriendStatus() { return this.mIsFriend; }
 
 }

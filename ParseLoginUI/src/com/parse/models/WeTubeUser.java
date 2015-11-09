fb9cb411cdab630976897a44667e70d95d12fbdd
Mark Wassef <markdevw@gmail.com>
2015-11-09 16:10:15 -0500

    Refactored WeTubeUser

--- ParseLoginUI/src/com/parse/models/WeTubeUser.java
+++ ParseLoginUI/src/com/parse/models/WeTubeUser.java
@@ -9,24 +9,14 @@ import org.json.JSONArray;
  */
 public class WeTubeUser extends ParseUser {
 
-    public WeTubeUser(){
+    public WeTubeUser(){ /*Do Nothing */ }
 
-    }
-
-    public WeTubeUser(boolean isLoggedIn, boolean isInSession){
-        setLoggedStatus(isLoggedIn);
-        setSessionStatus(isInSession);
-    }
-
-    public void setLoggedStatus(boolean isLoggedIn) {
-        put("isLoggedIn", isLoggedIn);
-    }
+    public void setLoggedStatus(boolean isLoggedIn) { put("isLoggedIn", isLoggedIn); }
     public boolean getLoggedStatus() { return getBoolean("isLoggedIn"); }
+
     public void setSessionStatus(boolean isInSession) { put("isInSession", isInSession); }
     public boolean getSessionStatus() { return getBoolean("isInSession"); }
-    public void setDefaultFriends(JSONArray array) { put("friends", array); }
+
     public void setDefaultTags(JSONArray array) { put("tags", array); }
-    public void setDefaultBlockedUsers(JSONArray array) { put("blockedUsers", array); }
-    public void setDefaultBlockedBy(JSONArray array) { put("blockedUsers", array); }
 }
 

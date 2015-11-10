ae0924927399663418630185466649157fdcf205
Mark Wassef <markdevw@gmail.com>
2015-11-09 19:29:52 -0500

    Refactored ProfileDialogFragment

--- app/src/main/java/com/gmail/markdevw/wetube/fragments/ProfileDialogFragment.java
+++ app/src/main/java/com/gmail/markdevw/wetube/fragments/ProfileDialogFragment.java
@@ -18,27 +18,20 @@ import com.gmail.markdevw.wetube.adapters.ProfileUncommonItemAdapter;
  * Created by Mark on 4/17/2015.
  */
 public class ProfileDialogFragment extends DialogFragment {
-
-    RecyclerView commonRecyclerView;
-    RecyclerView uncommonRecyclerView;
-    ProfileCommonItemAdapter profileCommonItemAdapter;
-    ProfileUncommonItemAdapter profileUncommonItemAdapter;
-
-    public ProfileDialogFragment() {
-
-    }
+    
+    public ProfileDialogFragment() { /* Do Nothing */ }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        getDialog().setTitle("Shared Tags");
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        getDialog().setTitle(R.string.shared_tags);
 
         View view = inflater.inflate(R.layout.fragment_profile_dialog, container);
-        commonRecyclerView= (RecyclerView) view.findViewById(R.id.rv_fragment_profile_dialog_common);
-        uncommonRecyclerView= (RecyclerView) view.findViewById(R.id.rv_fragment_profile_dialog_uncommon);
 
-        profileCommonItemAdapter = new ProfileCommonItemAdapter();
-        profileUncommonItemAdapter = new ProfileUncommonItemAdapter();
+        RecyclerView commonRecyclerView = (RecyclerView) view.findViewById(R.id.rv_fragment_profile_dialog_common);
+        RecyclerView uncommonRecyclerView = (RecyclerView) view.findViewById(R.id.rv_fragment_profile_dialog_uncommon);
+
+        ProfileCommonItemAdapter profileCommonItemAdapter = new ProfileCommonItemAdapter();
+        ProfileUncommonItemAdapter profileUncommonItemAdapter = new ProfileUncommonItemAdapter();
 
         commonRecyclerView.setLayoutManager(new LinearLayoutManager(WeTubeApplication.getSharedInstance()));
         commonRecyclerView.setItemAnimator(new DefaultItemAnimator());

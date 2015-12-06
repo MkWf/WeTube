eefa29974bd834d8a051d9f6abdf7684bf6de7b7
Mark Wassef <markdevw@gmail.com>
2015-12-06 12:38:38 -0500

    Added root entry, VideoItemContainer. Item class needs to be created

---
+++ app/src/main/java/com/gmail/markdevw/wetube/api/model/video/response/VideoItemContainer.java
@@ -0,0 +1,35 @@
+package com.gmail.markdevw.wetube.api.model.video.response;
+
+/**
+ * Created by Mark on 12/6/2015.
+ */
+public class VideoItemContainer {
+    private String nextPageToken;
+    private String prevPageToken;
+    
+    private List<Item> items;
+
+    public VideoItemContainer(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Item> getItems(){
+        return items;
+    }
+
+    public String getNextPageToken() {
+        return nextPageToken;
+    }
+
+    public void setNextPageToken(String nextPageToken) {
+        this.nextPageToken = nextPageToken;
+    }
+
+    public String getPrevPageToken() {
+        return prevPageToken;
+    }
+
+    public void setPrevPageToken(String prevPageToken) {
+        this.prevPageToken = prevPageToken;
+    }
+}

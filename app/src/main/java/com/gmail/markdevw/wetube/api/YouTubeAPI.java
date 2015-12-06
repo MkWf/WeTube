b55d9159ed125840a1e0a9b0f9f3483b7d636721
Mark Wassef <markdevw@gmail.com>
2015-12-06 17:48:17 -0500

    Added new method to YouTubeAPI to retrieve the length of each video. Renamed video package to video_response.

--- app/src/main/java/com/gmail/markdevw/wetube/api/YouTubeAPI.java
+++ app/src/main/java/com/gmail/markdevw/wetube/api/YouTubeAPI.java
@@ -17,4 +17,7 @@ public interface YouTubeAPI {
     @GET("/youtube/v3/search?part=id,snippet&type=video&key=AIzaSyDqalWrQoW2KoHoYLoyKl-FhncIQd2C3Rk" +
             "&fields=prevPageToken,nextPageToken,items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)&maxResults=50")
     Call<VideoItemContainer> getVideos(@Query("q") String search, @Query("pageToken") String pageToken);
+
+    @GET("/youtube/v3/videos?&key=AIzaSyDqalWrQoW2KoHoYLoyKl-FhncIQd2C3Rk&part=contentDetails&fields=items(contentDetails/duration)")
+    Call<VideoItemContainer> getDurations(@Query("id") String ids);
 }

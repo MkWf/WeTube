365643a3124140f95248e4c51dfa9e27c4870362
Mark Wassef <markdevw@gmail.com>
2015-12-06 12:52:45 -0500

    Added required YouTubeAPI methods

--- app/src/main/java/com/gmail/markdevw/wetube/api/YouTubeAPI.java
+++ app/src/main/java/com/gmail/markdevw/wetube/api/YouTubeAPI.java
@@ -1,13 +1,20 @@
 package com.gmail.markdevw.wetube.api;
 
+import com.gmail.markdevw.wetube.api.model.video.response.VideoItemContainer;
+
+import retrofit.Call;
 import retrofit.http.GET;
+import retrofit.http.Query;
 
 /**
  * Created by Mark on 12/6/2015.
  */
 public interface YouTubeAPI {
-    @GET("/youtube/v3/search?part=id,snippet&type=video&key=AIzaSyDqalWrQoW2KoHoYLoyKl-FhncIQd2C3Rk&fields=prevPageToken,nextPageToken,items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)&maxResults=50")
+    @GET("/youtube/v3/search?part=id,snippet&type=video&key=AIzaSyDqalWrQoW2KoHoYLoyKl-FhncIQd2C3Rk" +
+            "&fields=prevPageToken,nextPageToken,items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)&maxResults=50")
+    Call<VideoItemContainer> getVideos(@Query("q") String search);
 
-    @GET("/youtube/v3/search?part=id,snippet&type=video&key=AIzaSyDqalWrQoW2KoHoYLoyKl-FhncIQd2C3Rk&fields=prevPageToken,nextPageToken,items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)&maxResults=50")
-
+    @GET("/youtube/v3/search?part=id,snippet&type=video&key=AIzaSyDqalWrQoW2KoHoYLoyKl-FhncIQd2C3Rk" +
+            "&fields=prevPageToken,nextPageToken,items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)&maxResults=50")
+    Call<VideoItemContainer> getVideos(@Query("q") String search, @Query("pageToken") String pageToken);
 }

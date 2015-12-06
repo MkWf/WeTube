a637f5a4aefe4a48aab195cffc7a027b9383997a
Mark Wassef <markdevw@gmail.com>
2015-12-06 18:06:05 -0500

    Implemented DurationContainer

--- app/src/main/java/com/gmail/markdevw/wetube/api/model/video/duration_response/DurationContainer.java
+++ app/src/main/java/com/gmail/markdevw/wetube/api/model/video/duration_response/DurationContainer.java
@@ -1,8 +1,18 @@
 package com.gmail.markdevw.wetube.api.model.video.duration_response;
 
+import java.util.List;
+
 /**
  * Created by Mark on 12/6/2015.
  */
 public class DurationContainer {
+    private List<Item> items;
 
+    public DurationContainer(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Item> getItems(){
+        return items;
+    }
 }

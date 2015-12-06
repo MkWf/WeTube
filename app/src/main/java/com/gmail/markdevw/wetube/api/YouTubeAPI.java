efe4fa8d52acaccbcf757b0b0c0130602a759903
Mark Wassef <markdevw@gmail.com>
2015-12-06 12:35:45 -0500

    Added the paths that will be used to start a video search and page through it

--- app/src/main/java/com/gmail/markdevw/wetube/api/YouTubeAPI.java
+++ app/src/main/java/com/gmail/markdevw/wetube/api/YouTubeAPI.java
@@ -1,8 +1,13 @@
 package com.gmail.markdevw.wetube.api;
 
+import retrofit.http.GET;
+
 /**
  * Created by Mark on 12/6/2015.
  */
 public interface YouTubeAPI {
+    @GET("/youtube/v3/search?part=id,snippet&type=video&key=AIzaSyDqalWrQoW2KoHoYLoyKl-FhncIQd2C3Rk&fields=prevPageToken,nextPageToken,items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)&maxResults=50")
+
+    @GET("/youtube/v3/search?part=id,snippet&type=video&key=AIzaSyDqalWrQoW2KoHoYLoyKl-FhncIQd2C3Rk&fields=prevPageToken,nextPageToken,items(id/videoId,snippet/title,snippet/description,snippet/thumbnails/default/url)&maxResults=50")
     
 }

0fd0d27fa5297a982e4a4689626f52baf2dc3281
Mark Wassef <markdevw@gmail.com>
2016-03-24 11:57:56 -0400

    Added login package and LoginActivity

---
+++ app/src/main/java/com/gmail/markdevw/wetube/login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.gmail.markdevw.wetube.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Mark on 3/23/2016.
+ */
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
